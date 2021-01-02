54e6451a5e4b5b02bc3f67ad7e0a07a0b2b434b3
harsh raj <harshraj123gupta@gmail.com>
2021-01-02 11:12:43 +0530

    Dark mode

--- app/src/main/java/com/dev/ieee_nsut/MainActivity.java
+++ app/src/main/java/com/dev/ieee_nsut/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity
     private static final String DEVELOPERS_FRAGMENT_TAG = "developers_fragment";
 
     private NavigationView mNavigationView;
-//    private Button btnToggleDark;
+    private Button btnToggleDark;
     private String currentFragmentTag = "";
 
     @Override
@@ -82,87 +82,87 @@ public class MainActivity extends AppCompatActivity
             mNavigationView.setCheckedItem(R.id.nav_home);
         }
 
-//        Button btnToggleDark = findViewById(R.id.btnToggleDark);
-//
-//        // Saving state of our app
-//        // using SharedPreferences
-//        SharedPreferences sharedPreferences
-//                = getSharedPreferences(
-//                "sharedPrefs", MODE_PRIVATE);
-//        final SharedPreferences.Editor editor
-//                = sharedPreferences.edit();
-//        final boolean isDarkModeOn
-//                = sharedPreferences
-//                .getBoolean(
-//                        "isDarkModeOn", false);
-//
-//        // When user reopens the app
-//        // after applying dark/light mode
-//        if (isDarkModeOn) {
-//            AppCompatDelegate
-//                    .setDefaultNightMode(
-//                            AppCompatDelegate
-//                                    .MODE_NIGHT_YES);
-//            btnToggleDark.setText(
-//                    "Disable Dark Mode");
-//        }
-//        else {
-//            AppCompatDelegate
-//                    .setDefaultNightMode(
-//                            AppCompatDelegate
-//                                    .MODE_NIGHT_NO);
-//            btnToggleDark
-//                    .setText(
-//                            "Enable Dark Mode");
-//        }
-//
-//        btnToggleDark.setOnClickListener(
-//                new View.OnClickListener() {
-//
-//                    @Override
-//                    public void onClick(View view)
-//                    {
-//                        // When user taps the enable/disable
-//                        // dark mode button
-//                        if (isDarkModeOn) {
-//
-//                            // if dark mode is on it
-//                            // will turn it off
-//                            AppCompatDelegate
-//                                    .setDefaultNightMode(
-//                                            AppCompatDelegate
-//                                                    .MODE_NIGHT_NO);
-//                            // it will set isDarkModeOn
-//                            // boolean to false
-//                            editor.putBoolean(
-//                                    "isDarkModeOn", false);
-//                            editor.apply();
-//
-//                            // change text of Button
-//                            btnToggleDark.setText(
-//                                    "Enable Dark Mode");
-//                        }
-//                        else {
-//
-//                            // if dark mode is off
-//                            // it will turn it on
-//                            AppCompatDelegate
-//                                    .setDefaultNightMode(
-//                                            AppCompatDelegate
-//                                                    .MODE_NIGHT_YES);
-//
-//                            // it will set isDarkModeOn
-//                            // boolean to true
-//                            editor.putBoolean(
-//                                    "isDarkModeOn", true);
-//                            editor.apply();
-//
-//                            // change text of Button
-//                            btnToggleDark.setText(
-//                                    "Disable Dark Mode");
-//                        }
-//                    }
-//                });
+        Button btnToggleDark = findViewById(R.id.btnToggleDark);
+
+        // Saving state of our app
+        // using SharedPreferences
+        SharedPreferences sharedPreferences
+                = getSharedPreferences(
+                "sharedPrefs", MODE_PRIVATE);
+        final SharedPreferences.Editor editor
+                = sharedPreferences.edit();
+        final boolean isDarkModeOn
+                = sharedPreferences
+                .getBoolean(
+                        "isDarkModeOn", false);
+
+        // When user reopens the app
+        // after applying dark/light mode
+        if (isDarkModeOn) {
+            AppCompatDelegate
+                    .setDefaultNightMode(
+                            AppCompatDelegate
+                                    .MODE_NIGHT_YES);
+            btnToggleDark.setText(
+                    "Disable Dark Mode");
+        }
+        else {
+            AppCompatDelegate
+                    .setDefaultNightMode(
+                            AppCompatDelegate
+                                    .MODE_NIGHT_NO);
+            btnToggleDark
+                    .setText(
+                            "Enable Dark Mode");
+        }
+
+        btnToggleDark.setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view)
+                    {
+                        // When user taps the enable/disable
+                        // dark mode button
+                        if (isDarkModeOn) {
+
+                            // if dark mode is on it
+                            // will turn it off
+                            AppCompatDelegate
+                                    .setDefaultNightMode(
+                                            AppCompatDelegate
+                                                    .MODE_NIGHT_NO);
+                            // it will set isDarkModeOn
+                            // boolean to false
+                            editor.putBoolean(
+                                    "isDarkModeOn", false);
+                            editor.apply();
+
+                            // change text of Button
+                            btnToggleDark.setText(
+                                    "Enable Dark Mode");
+                        }
+                        else {
+
+                            // if dark mode is off
+                            // it will turn it on
+                            AppCompatDelegate
+                                    .setDefaultNightMode(
+                                            AppCompatDelegate
+                                                    .MODE_NIGHT_YES);
+
+                            // it will set isDarkModeOn
+                            // boolean to true
+                            editor.putBoolean(
+                                    "isDarkModeOn", true);
+                            editor.apply();
+
+                            // change text of Button
+                            btnToggleDark.setText(
+                                    "Disable Dark Mode");
+                        }
+                    }
+                });
     }
 
     //When pressed back activity should jump to HomeFragment
